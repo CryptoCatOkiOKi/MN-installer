cf732e58309b1027d90f3511cce0afa4b21bdaec
AnonRobot <james.woodbridge@gmail.com>
2018-05-13 20:04:37 +0100

    Added unpacking of the zip files

--- mn_install.sh
+++ mn_install.sh
@@ -54,6 +54,19 @@ apt install -y software-properties-common
 add-apt-repository ppa:bitcoin/bitcoin
 apt update
 apt -y install build-essential libtool autotools-dev automake pkg-config libssl-dev autoconf libssl-dev libgmp3-dev libevent-dev bsdmainutils libboost-all-dev libdb4.8-dev libdb4.8++-dev nano git libminiupnpc-dev libzmq5
+
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 cd
+
+wget https://github.com/Dystem/dystem-core/releases/download/V1.0.2/mnbin.tar.gz
+tar -xzf mnbin.tar.gz
+
 #replace with pre compiled binaries
 #git clone https://github.com/Dystem/dystem-core.git 
